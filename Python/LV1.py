027f709571e2630d2faa0da319628f55ecd35425
I589040 <jonas.giessler@sap.com>
2024-03-07 23:40:38 +0100

    Added Python

--- Python/LV1.py
+++ Python/LV1.py
@@ -1,9 +1,14 @@
 import pygame
 import sys
 from PIL import Image
+import time
+import math
 
 pygame.init()
-pygame.mouse.set_visible(False)
+pygame.mixer.init()
+pygame.mixer.music.load("/Users/i589040/Documents/GitHub/Spiel-Info-Q11-2022/Sounds/Background/696485__gis_sweden__minimal-tech-background-music-mtbm02.wav") 
+pygame.mixer.music.play(-1,0.0)
+# pygame.mouse.set_visible(False)
 info_object = pygame.display.Info()
 screen_size = (info_object.current_w, info_object.current_h)
 
@@ -26,7 +31,7 @@ class ObstacleMap:
         self.image = pygame.transform.scale(self.image, (11200, 1080))
 
     def collides_horizontally_right(self, rect:pygame.Rect):
-        for y in range(rect.y, rect.y + rect.h):
+        for y in range(rect.y+25, rect.y + rect.h-25):
             try:
                 if self.image.get_at((rect.x + rect.w, y)) != (0, 0, 0, 0):
                     return True
@@ -35,7 +40,7 @@ class ObstacleMap:
         return False
     
     def collides_horizontally_left(self, rect:pygame.Rect):
-        for y in range(rect.y, rect.y + rect.h):
+        for y in range(rect.y+25, rect.y + rect.h-25):
             try:
                 if self.image.get_at((rect.x, y)) != (0, 0, 0, 0):
                     return True
@@ -56,8 +61,15 @@ class ObstacleMap:
     
     def collides_vertically_bottom(self, rect):
         for x in range(rect.x+4, rect.x + rect.w-4):
-            try:
+            try: 
                 if self.image.get_at((x, rect.y + rect.h)) != (0, 0, 0, 0):
+                    for y in range(int(rect.y+rect.h*0.5), rect.y + rect.h):
+                            if self.image.get_at((rect.x+rect.w-10, y)) != (0, 0, 0, 0):
+                                player.y = y-rect.h
+                                break
+                            if self.image.get_at((rect.x+10, y)) != (0, 0, 0, 0):
+                                player.y = y-rect.h
+                                break
                     return True
             except IndexError:
                 pass
@@ -95,7 +107,7 @@ class Player:
     def __init__(self):
         self.x, self.y = screen_size[0] // 2, screen_size[1] // 2
         self.dy = 0
-        self.is_jumping = False
+        self.jump_enabled = True
         self.gravity = 0.3
         self.jump_height = 12
         self.scale = 0.15
@@ -106,6 +118,9 @@ class Player:
         self.walking_right = False
         self.walking_left = False
         self.crouch = False
+        self.jumping_sound = pygame.mixer.Sound("/Users/i589040/Documents/GitHub/Spiel-Info-Q11-2022/Sounds/Player/399095__plasterbrain__8bit-jump.flac")
+        self.jumping_sound.set_volume(0.8)
+        self.last_jump = time.time()
 
     @staticmethod
     def load_gif(path, scale):
@@ -136,10 +151,12 @@ class Player:
         surface.blit(image, (self.x, self.y))  
 
     def jump(self):
-        test_rect = pygame.Rect(player.x - world.x, player.y, player.rect.w, player.rect.h).move(0, self.dy)
-        if obstacle_map.collides_vertically_bottom(test_rect):
-            self.is_jumping = True
+        test_rect = pygame.Rect(player.x - world.x, player.y, player.rect.w, player.rect.h).move(0, 2*self.dy)
+        if obstacle_map.collides_vertically_bottom(test_rect) and self.jump_enabled:
             self.dy -= self.jump_height
+            if (time.time() - self.last_jump) > 0.3:
+                self.last_jump = time.time()
+                pygame.mixer.Sound.play(self.jumping_sound)
 
     def update(self):
         self.prev_x, self.prev_y = self.x, self.y  # Remember previous position
@@ -152,9 +169,8 @@ class Player:
 
         if not obstacle_map.collides_vertically_bottom(test_rect):
             self.y += self.dy
-        else:
+        elif not self.dy < 0:
             self.dy = 0
-            self.is_jumping = False
 
         if(self.walking_right):
             self.current_frame = (self.current_frame + gs.dt_last_frame/4 * 1) % (len(self.animation_frames))
@@ -162,6 +178,58 @@ class Player:
             self.current_frame = (self.current_frame + gs.dt_last_frame/4 * 1) % (len(self.animation_frames))
         else:
             self.current_frame = 3
+
+def get_rotation_angle(velocity):
+    # velocity[0] is horizontal speed
+    # velocity[1] is vertical speed
+    if velocity[0]==0: # this conditional logic is done in order to handle the case whenever horizontal speed becomes zero to avoid zero division error
+        return 90 if velocity[1]>0 else 270
+    angle_in_radians = math.atan(velocity[1]/velocity[0])
+    # convert radian to degree
+    angle_in_degree = math.degrees(angle_in_radians)
+    #Finding appropriate quadrant of angle
+    if velocity[0]>0 and velocity[1]>0:
+        final_angle = angle_in_degree
+    elif velocity[0]<0 and velocity[1]>0:
+        final_angle = 180 + angle_in_degree
+    elif velocity[0]<0 and velocity[1]<0:
+        final_angle = 180 + angle_in_degree
+    elif velocity[0]>0 and velocity[1]<0:
+        final_angle = 360 + angle_in_degree
+    else:
+        final_angle = angle_in_degree
+    return final_angle
+
+class shot:
+    shots_list = []
+    def __init__(self):
+        shot.shots_list.append(self)
+        self.image = pygame.image.load("/Users/i589040/Documents/GitHub/Spiel-Info-Q11-2022/Bilder/Objekte/PNG/Foreground/Hindernisse/Container_Side_1.png")
+        self.image = pygame.transform.scale(self.image, (50,10))
+        self.velocity = self.set_velocities()
+        v = -get_rotation_angle(self.velocity)
+        self.image = pygame.transform.rotate(self.image,v)
+        self.coordinates = [player.x + player.rect.w/2 - world.x, player.y + player.rect.h/2]
+    
+    def move(self):
+        self.coordinates[0] += self.velocity[0]
+        self.coordinates[1] += self.velocity[1]
+
+        test_rect = pygame.Rect(player.x - world.x, player.y, player.rect.w, player.rect.h).move(1, 0)
+
+
+        
+
+    def set_velocities(self) -> list:
+        x,y = pygame.mouse.get_pos()
+        v = [x - (player.x + player.rect.w/2), y - (player.y + player.rect.h/2)]
+        v_l = math.sqrt(v[0]**2 + v[1]**2)
+        v = [(i/v_l)*10 for i in v]
+        return v
+    
+    def draw(self,surface):
+        surface.blit(self.image, (self.coordinates[0] + world.x, self.coordinates[1]))  
+
 
 
 world = World()
@@ -182,10 +250,13 @@ while gs.running:
                 gs.running = False
                 pygame.quit()
                 sys.exit()
-            if event.key == pygame.K_SPACE:
-                player.jump()
-
+        if event.type == pygame.MOUSEBUTTONUP:
+            shot()
     # In the game loop
+    if keys[pygame.K_SPACE]:
+        if not player.dy < 0:
+            player.jump()
+
     if keys[pygame.K_d]:
             test_rect = pygame.Rect(player.x - world.x, player.y, player.rect.w, player.rect.h).move(1, 0)
             if not obstacle_map.collides_horizontally_right(test_rect):
@@ -205,13 +276,20 @@ while gs.running:
 
     if keys[pygame.K_LSHIFT]:
         player.crouch = True
+        player.jump_enabled = False
     elif player.crouch == True:
+        player.jump_enabled = True
         player.y -= player.rect.size[1]
         player.crouch = False
+
+
 
     player.update()
     world.draw(display)
     player.draw(display)
+    for shots in shot.shots_list:
+        shots.move()
+        shots.draw(display)
 
     pygame.display.flip()
     gs.dt_last_frame = FPS.tick()/17
